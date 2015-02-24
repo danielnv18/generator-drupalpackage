a3158ca492cab75011e0dcae191509f2a5957808
Daniel Noyola <danielnv18@gmail.com>
2015-02-23 19:02:40 -0600

    Fixed SASS compling.

--- app/index.js
+++ app/index.js
@@ -88,17 +88,17 @@ module.exports = yeoman.generators.Base.extend({
       this.fs.copyTpl(
         this.templatePath('profile/info'),
         this.destinationPath('src/profiles/' + this.projectName + '/' + this.projectName + '.info'),
-      { projectName: this.projectName }
+        { projectName: this.projectName }
       );
       this.fs.copyTpl(
         this.templatePath('profile/install'),
         this.destinationPath('src/profiles/' + this.projectName + '/' + this.projectName + '.install'),
-      { projectName: this.projectName }
+        { projectName: this.projectName }
       );
       this.fs.copyTpl(
         this.templatePath('profile/profile'),
         this.destinationPath('src/profiles/' + this.projectName + '/' + this.projectName + '.profile'),
-      { projectName: this.projectName }
+        { projectName: this.projectName }
       );
     },
 
@@ -107,37 +107,67 @@ module.exports = yeoman.generators.Base.extend({
     },
 
     theme: function() {
-      this.bulkDirectory('sass', 'src/sass');
+      this.bulkDirectory('sass/abstractions', 'src/sass/abstractions');
+      this.bulkDirectory('sass/base', 'src/sass/base');
+      this.bulkDirectory('sass/components', 'src/sass/components');
+      this.bulkDirectory('sass/variables', 'src/sass/variables');
+
+      this.fs.copy(
+        this.templatePath('sass/hacks.scss'),
+        this.destinationPath('src/sass/' + this.projectName + '.hacks.scss')
+      );
+      this.fs.copyTpl(
+        this.templatePath('sass/no-query.scss'),
+        this.destinationPath('src/sass/' + this.projectName + '.no-query.scss'),
+        { projectName: this.projectName }
+      );
+      this.fs.copy(
+        this.templatePath('sass/normalize.scss'),
+        this.destinationPath('src/sass/' + this.projectName + '.normalize.scss')
+      );
+      this.fs.copy(
+        this.templatePath('sass/styles.scss'),
+        this.destinationPath('src/sass/' + this.projectName + '.styles.scss')
+      );
+
       this.bulkDirectory("theme", "src/themes/" + this.projectName);
 
       this.fs.copyTpl(
         this.templatePath('theme_tpl/_page.preprocess.inc'),
         this.destinationPath('src/themes/' + this.projectName + '/preprocess/page.preprocess.inc'),
-      { projectName: this.projectName }
+        { projectName: this.projectName }
       );
 
       this.fs.copyTpl(
         this.templatePath('theme_tpl/_page.process.inc'),
         this.destinationPath('src/themes/' + this.projectName + '/process/page.process.inc'),
-      { projectName: this.projectName }
+        { projectName: this.projectName }
       );
 
       this.fs.copyTpl(
         this.templatePath('theme_tpl/info'),
         this.destinationPath('src/themes/' + this.projectName + '/' + this.projectName + '.info'),
-      { projectName: this.projectName }
+        { projectName: this.projectName }
       );
       this.fs.copyTpl(
         this.templatePath('theme_tpl/_template.php'),
         this.destinationPath('src/themes/' + this.projectName + '/template.php'),
-      { projectName: this.projectName }
+        { projectName: this.projectName }
       );
       this.fs.copyTpl(
         this.templatePath('theme_tpl/_theme-settings.php'),
         this.destinationPath('src/themes/' + this.projectName + '/theme-settings.php'),
-      { projectName: this.projectName }
+        { projectName: this.projectName }
       );
-
+      this.fs.copyTpl(
+        this.templatePath('_config.rb'),
+        this.destinationPath('config.rb'),
+        { projectName: this.projectName }
+      );
+      this.fs.copy(
+        this.templatePath('_Gemfile'),
+        this.destinationPath('Gemfile')
+      );
     }
 
   },
